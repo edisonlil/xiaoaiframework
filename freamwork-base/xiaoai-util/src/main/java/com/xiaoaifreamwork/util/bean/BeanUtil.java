a1fa1d281073ae662330b8202e2b403ec3f779ac
edison <edisonlil@163.com>
2020-11-10 15:51:33 +0800

    init

--- freamwork-base/xiaoai-util/src/main/java/com/xiaoaifreamwork/util/bean/BeanUtil.java
+++ freamwork-base/xiaoai-util/src/main/java/com/xiaoaifreamwork/util/bean/BeanUtil.java
@@ -9,12 +9,28 @@ import java.util.Map;
 public class BeanUtil {
 
 
+    /**
+     * 对象转Map
+     *
+     * @param bean              bean对象
+     * @param isToUnderlineCase 是否转换为下划线模式
+     * @param ignoreNullValue   是否忽略值为空的字段
+     * @return Map
+     */
     public static Map<String, Object> beanToMap(Object bean, boolean isToUnderlineCase, boolean ignoreNullValue){
         return null;
     }
-
+    
+    /**
+     * Map转换为Bean对象
+     *
+     * @param <T>           Bean类型
+     * @param map           {@link Map}
+     * @param beanClass     Bean Class
+     * @param isIgnoreError 是否忽略注入错误
+     * @return Bean
+     */
     public static <T> T mapToBean(Map<?, ?> map, Class<T> beanClass, boolean isIgnoreError){
-
         return null;
     }
 }
